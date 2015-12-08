258c9c722a71101ecfb66c825db8f68211c521a3
Matthew Norvell <zexelesque@gmail.com>
2015-12-08 08:23:59 -0800

    Add stop go button

--- Mother/Mother.js
+++ Mother/Mother.js
@@ -8,6 +8,15 @@ function backToMother() {
 function meMum() {
     document.getElementById("mum").innerHTML = "Me Mum";
     };
+function stahp() {
+	document.getElementById("mum").id = "stahped";
+	document.getElementById("stahped").innerHTML = "&nbsp;";
+	document.getElementById("button").innerHTML = "<button onclick='go()'>Go</button>";
+	};
+function go() {
+	document.getElementById("stahped").id = "mum";
+	document.getElementById("button").innerHTML = "<button onclick='stahp()'>Stahp</button>";
+	};
     setInterval(yourMother, 1000)
     setInterval(backToMother, 850)
     setInterval(meMum, 600)
